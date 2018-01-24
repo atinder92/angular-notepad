e4d33a938bf3843f076da664f92c66399a84e6cc
Atinder Singh <atinder.s.92@gmail.com>
2018-01-24 21:01:44 +1100

    Created Saved Notes component

---
+++ angular-notepad/src/app/saved-notes/saved-notes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-notes',
+  templateUrl: './saved-notes.component.html',
+  styleUrls: ['./saved-notes.component.css']
+})
+export class SavedNotesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
