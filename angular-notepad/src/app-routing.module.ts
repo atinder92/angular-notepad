556be3e9e0d875aeeb69c841897462a55c6d614b
Atinder Singh <atinder.s.92@gmail.com>
2018-01-27 22:27:54 +1100

    Added PageNotFoundComponent to routing

--- angular-notepad/src/app-routing.module.ts
+++ angular-notepad/src/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes,RouterModule } from "@angular/router";
 import { CreateNoteComponent } from "./app/create-note/create-note.component";
 import { TrashComponent } from "./app/trash/trash.component";
 import { SavedNotesComponent } from "./app/saved-notes/saved-notes.component";
+import { PageNotFoundComponent } from "./app/page-not-found/page-not-found.component";
 
 //components
 
 const appRoutes: Routes = [
     {path:'',component:CreateNoteComponent},
     {path:'trash',component:TrashComponent},
-    {path:'notes',component:SavedNotesComponent}
+    {path:'notes',component:SavedNotesComponent},
+    { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
     imports:[RouterModule.forRoot(appRoutes)],
