10db6f7a63e0e868d1c743152699ff7217f5e570
Atinder Singh <atinder.s.92@gmail.com>
2018-01-28 22:59:11 +1100

    Added sign-in and sign-up components to routing

--- angular-notepad/src/app-routing.module.ts
+++ angular-notepad/src/app-routing.module.ts
@@ -4,6 +4,8 @@ import { CreateNoteComponent } from "./app/create-note/create-note.component";
 import { TrashComponent } from "./app/trash/trash.component";
 import { SavedNotesComponent } from "./app/saved-notes/saved-notes.component";
 import { PageNotFoundComponent } from "./app/page-not-found/page-not-found.component";
+import { SignupFormComponent } from "./app/signup-form/signup-form.component";
+import { SigninFormComponent } from "./app/signin-form/signin-form.component";
 
 //components
 
@@ -11,6 +13,8 @@ const appRoutes: Routes = [
     {path:'',component:CreateNoteComponent},
     {path:'trash',component:TrashComponent},
     {path:'notes',component:SavedNotesComponent},
+    {path:'signup',component:SignupFormComponent},
+    {path:'signin',component:SigninFormComponent},
     { path: '**', component: PageNotFoundComponent }
 ];
 @NgModule({
