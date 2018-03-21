5cf04a5b0ad321574b8f5bd66da3c100e23a0e81
Atinder Singh <atinder.s.92@gmail.com>
2018-03-21 23:13:27 +1100

    functionality to clear notes

--- angular-notepad/src/app/create-note/create-note.component.ts
+++ angular-notepad/src/app/create-note/create-note.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NoteAppService } from "../../services/note-app.service";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-create-note',
@@ -8,16 +9,26 @@ import { NoteAppService } from "../../services/note-app.service";
 })
 export class CreateNoteComponent implements OnInit {
 
-  private noteString:string;
+  private noteInput:string;
+  private noteCreatedSuccess:boolean = false;
+  @ViewChild('noteStringRef') textareaElement:ElementRef; 
   constructor(private noteAppService:NoteAppService) { }
 
   ngOnInit() {
 
   }
-
+   
   saveNote(){
     var currentDate = new Date();
-    this.noteAppService.addNote(this.noteString,currentDate);
+    this.noteAppService.addNote(this.noteInput,currentDate);
+    this.noteCreatedSuccess = true;
     this.noteAppService.logNotes();
   }
+
+  //Resets the noteInput to original state
+  newNote(noteForm:NgForm){
+    noteForm.controls["noteInput"].reset();
+    this.noteCreatedSuccess = false;
+  }
+
 }
