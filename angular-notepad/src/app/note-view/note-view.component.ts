247f65b96c627f2d559138604cf8c6863ff3fc53
Atinder Singh <atinder.s.92@gmail.com>
2018-03-22 23:37:26 +1100

    Displaying date in note-view component

--- angular-notepad/src/app/note-view/note-view.component.ts
+++ angular-notepad/src/app/note-view/note-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit,Input } from '@angular/core';
 import { Note } from '../../model/note.model';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-note-view',
